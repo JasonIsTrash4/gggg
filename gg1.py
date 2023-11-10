b07514fdf091bd7067046096fe137d99c9343f34
mps_11 <113040390+JasonIsTrash4@users.noreply.github.com>
2023-11-10 17:45:30 +0300

    Add files via upload

--- gg1.py
+++ gg1.py
@@ -1,17 +1,21 @@
 import turtle
 turtle.shape('turtle')
-turtle.speed(0)
-def draw_square(side, color):
+turtle.speed(1)
+turtle.fillcolor('')      
+def draw_square(x, color):
     counter = 0
     while counter < 4:
-        pencolor(color)
-        turtle.fd(side)
+        base_color='green'
+        turtle.fd(x)
         turtle.right(90)
-        counter +=1
+        counter += 1
 
-draw_square(100, "green")
-draw_square(110, "red")
-draw_square(120, blue)
-draw_square(130, purple)
+draw_square(100, 'green')
+turtle.fd(100)
+turtle.left(135)   
+turtle.fd(80)
+turtle.left(98)
+turtle.fd(76)
+turtle.goto()
 turtle.done()
 
